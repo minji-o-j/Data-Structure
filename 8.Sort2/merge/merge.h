35b34512f005f2d64b87769709eede088c5a48d3
minji-o-j <jminji98@gmail.com>
2020-06-20 00:12:30 +0900

    merge

---
+++ 8.Sort2/merge/merge.h
@@ -0,0 +1,11 @@
+// ======================================================================
+
+#define MAX_LIST_LENGTH         8
+
+// ======================================================================
+
+void merge_sort_log(int list[], int left, int right);
+void merge_sort(int list[], int left, int right);
+void merge(int list[], int left, int mid, int right);
+
+// ======================================================================
